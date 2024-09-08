26ba7819e3356030c88708a8750945ece4a2f53f
Regivan Souza <63743960+REGIVAN33@users.noreply.github.com>
2024-09-08 00:03:43 -0300

    Add files via upload

---
+++ js/dados.js
@@ -0,0 +1,30 @@
+let dados = [
+    {
+        titulo: "Google Gemini",
+        descricao: "A Imersão Dev da Alura com Google Gemini é uma iniciativa inovadora que visa capacitar desenvolvedores a utilizarem a inteligência artificial em seus projetos. Essa parceria entre a Alura e o Google oferece uma oportunidade única de aprender a programar e a integrar o Google Gemini em suas aplicações.",
+        link: "https://gemini.google.com",
+        tags: "Imersão Dev, Alura, Google Gemini, inteligência artificial, programação, desenvolvimento de aplicações, aprendizado online"
+    },
+    {
+        titulo: "Rafaella Ballerini",
+        descricao: "Desenvolvedora de software, instrutora e criadora de conteúdo. Possui um canal com mais de 240 mil pessoas inscritas, onde ensina sobre os mais diversos temas relacionados a programação e tecnologia, como HTML, CSS, Javascript, Git, Github... É instrutora front-end na plataforma da Alura e também na Imersão Dev, projeto que leva o ensino inicial de programação para todo o Brasil gratuitamente.",
+        link: "https://br.linkedin.com/in/rafaella-ballerini-45875016a",
+        tags: "Programação, UX & Design, Data Science, Front-end"
+    },
+    {
+        titulo: "Guilherme Lima",
+        descricao: "Guilherme é desenvolvedor de software formado em Sistemas de Informação e possui experiência em programação usando diferentes tecnologias como Python, Javascript e Go. Criador de mais de 30 cursos de diferentes áreas da plataforma com foco no treinamento de profissionais de TI, como Data Science, Python para web com Django e Django Rest, jogos com Javascript, Infraestrutura como código com Terraform e Ansible, Orientação a Objetos com Go. Além disso, é um dos instrutores da Imersão Dev da Alura.",
+        link: "https://www.linkedin.com/in/guilherme-lima-developer/",
+        tags: "Tech Educator, Professor na Alura, USP"
+    },
+    { 
+        titulo: "Luciano Martins",
+        descricao: "Luciano é um Developer Advocate para o Google Cloud, apaixonado por abordar problemas complexos com soluções criativas. Ele defende que engenheiros e desenvolvedores criem soluções com IA, Machine Learning e serviços de IA do Google Cloud. Quando não está brincando com coisas de IA, Luciano está tentando criar a próxima melhor cerveja caseira do mundo.",
+        link: "https://www.linkedin.com/in/lucianommartins/",
+        tags: "Aprendizado de máquina inteligência artificial ML/IA"
+    },
+    {        titulo: "Alura",
+        descricao: "A Alura compõe o maior ecossistema de ensino em tecnologia do Brasil e tem como missão transformar vidas e carreiras.",
+        link: "https://www.alura.com.br/",
+        tags: "Curso, Aluno, Professor, HTML, CSS, JavaScript, Imersão Dev_"
+    }
